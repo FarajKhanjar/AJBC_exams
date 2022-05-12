e68cc3be5f7e0da1500aaafd5f1a8f2c09670f3b
FarajKhanjar <farajkhanjar@gmail.com>
2022-05-12 14:30:49 +0300

    Create Tent class and the Orginal list

--- exam_4/src/exam_4/classes/Tent.java
+++ exam_4/src/exam_4/classes/Tent.java
@@ -1,5 +1,25 @@
 package exam_4.classes;
 
-public class Tent {
+public class Tent 
+{
+	//Fields:
+	private int numPeople;
+	private float width;
+	private float length;
+	private float height;
+	
+	//Constructor:
+	public Tent(int numPeople, float width, float length, float height) 
+	{
+		this.numPeople = numPeople;
+		this.width = width;
+		this.length = length;
+		this.height = height;
+	}
+	
+
+	
+	
+	
 
 }
--- exam_4/src/exam_4/runner/Runner.java
+++ exam_4/src/exam_4/runner/Runner.java
@@ -1,10 +1,38 @@
 package exam_4.runner;
 
-public class Runner {
+import java.util.ArrayList;
+import java.util.List;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import exam_4.classes.Tent;
 
+public class Runner 
+{
+	static List<Tent> tentsList = new ArrayList<>();
+	
+	public static void main(String[] args) 
+	{
+		createAllTentsList();
+		createCopyListAndSortedByArea();
+		
+	}
+	
+	public static void createAllTentsList()
+	{
+		tentsList.add(new Tent(5,2.5f,2f,3f));
+		tentsList.add(new Tent(4,3f,2.5f,3f));
+		tentsList.add(new Tent(10,6f,5.5f,2f));
+		tentsList.add(new Tent(5,5.5f,2f,2.5f));
+		tentsList.add(new Tent(7,3.5f,4f,3f));
+		tentsList.add(new Tent(10,2.5f,2f,3f));
+		tentsList.add(new Tent(3,1.5f,3f,3f));
+		tentsList.add(new Tent(2,1f,2f,2f));
+		tentsList.add(new Tent(3,1.5f,2f,3f));
+		tentsList.add(new Tent(4,2f,2.5f,2.5f));	
+	}
+	
+	public static List<Tent> createAllTentsList(List<Tent> orginalList)
+	{
+	
 	}
 
 }
